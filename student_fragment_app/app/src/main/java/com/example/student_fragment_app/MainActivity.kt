4ce7719b9266f63bcf45cea4efbe2f18c5e659a5
pranesh236 <230701236@rajalakshmi.edu.in>
2026-03-20 11:28:19 +0530

    Added Student Fragment App

---
+++ student_fragment_app/app/src/main/java/com/example/student_fragment_app/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.student_fragment_app
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnBasicDetails: Button = findViewById(R.id.btnBasicDetails)
+        val btnStudentMarks: Button = findViewById(R.id.btnStudentMarks)
+
+        // Load default fragment
+        loadFragment(StudentBasicDetailsFragment())
+
+        btnBasicDetails.setOnClickListener {
+            loadFragment(StudentBasicDetailsFragment())
+        }
+
+        btnStudentMarks.setOnClickListener {
+            loadFragment(StudentMarkFragment())
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.commit()
+    }
+}
